0695977cb4b3bd223be245c900c1acb3d885b5e7
Jessi Mora <jessimora2612@gmail.com>
2023-09-05 21:28:28 -0400

    descomentando react.StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import { CalendarApp } from "./CalendarApp";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-      <CalendarApp />
-    </BrowserRouter>
-  </Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <CalendarApp />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
